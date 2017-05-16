ff25a0aa62fddef4a8253e2d76795ee7b0a84021
Stas Hladki <stas.hladki@gmail.com>
2017-05-16 16:56:18 +0300

    Fix AOT stuff and module

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 export {NgxTableSortableDirective} from './src/ngx-table-sortable.directive'
 export {SORTING_ORDER} from './src/sorting-order'
 export {SortingSettings} from './src/sorting-settings'
+export {NgxTableSortableModule} from './src/ngx-table-sortable.module'
---
+++ src/ngx-table-sortable.module.ts
@@ -0,0 +1,8 @@
+import {NgModule} from "@angular/core";
+import {NgxTableSortableDirective} from "./ngx-table-sortable.directive";
+
+@NgModule({
+    declarations: [NgxTableSortableDirective],
+    exports: [NgxTableSortableDirective]
+})
+export class NgxTableSortableModule {}
